d2d43bf8740698330470895ede0f2e31f6744a2a
CryptKid <CryptKid@users.noreply.github.com>
2018-05-10 22:32:11 +0200

    Three improvements: -Added an input-area -Translated software to english -Added prebuild jar for quick setup

--- NumberSort/src/mainApp.java
+++ NumberSort/src/mainApp.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class mainApp {
     private JButton generateNumberButton;
@@ -13,9 +15,11 @@ public class mainApp {
     private JButton startSortButton;
     private JLabel label4;
     private JButton resetButton;
+    private JButton fromList;
+    private JTextArea input;
 
     private static int arrayNumber = 0;
-    private static int[] numbers = new int[30];
+    private static int[] numbers;
     private static int sortType = 0;
 
     private mainApp() {
@@ -64,10 +68,18 @@ public class mainApp {
                 uiHandler(0);
             }
         });
+        fromList.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                inputFromList();
+                uiHandler(1);
+            }
+        });
     }
 
     private void generateNumber(){
-        ConsoleOutputTextArea.append("Reihenfolge: ");
+        numbers = new int[30];
+        ConsoleOutputTextArea.append("Order: ");
         for(int i = 0; i < 30; i++){
             int generatedNumber = (int) (Math.random() * 100 + 1);
             numbers[arrayNumber] = generatedNumber;
@@ -75,6 +87,53 @@ public class mainApp {
             ConsoleOutputTextArea.append(generatedNumber + ", ");
         }
         ConsoleOutputTextArea.append(System.lineSeparator());
+    }
+
+    private void inputFromList(){
+        ConsoleOutputTextArea.append("Order: ");
+        String[] lines = input.getText().split("\\n");
+        String tmp;
+        int[] numbero = new int[lines.length + 1];
+        numbers = new int[lines.length];
+        arrayNumber = lines.length;
+        for (int i = 0; i <lines.length ; i++) {
+            try {
+                numbero[i] = Integer.parseInt(lines[i]);
+            } catch(Throwable e) {
+                numbero[i] = 0;
+            }
+            numbers = numbero;
+        }
+        for (int i = 0; i < lines.length; i++) {
+            ConsoleOutputTextArea.append(String.valueOf(numbers[i]));
+            ConsoleOutputTextArea.append(", ");
+        }
+        ConsoleOutputTextArea.append((System.lineSeparator()));
+        /*for(int i = 0; i < input.getLineCount(); i++){
+            try {
+                int generatedNumber = Integer.parseInt(input.get;
+            } catch (ParseException e) {
+
+            };
+
+            numbers[arrayNumber] = generatedNumber;
+            arrayNumber = arrayNumber+1;
+            ConsoleOutputTextArea.append(generatedNumber + ", ");
+        }
+        ConsoleOutputTextArea.append(System.lineSeparator());
+        //String s[] = input.getText().split("\\r?\\n");
+        //int[] numbero = new int[s.length];
+        String iput = input.getText();
+        String[] ipt = iput.split("\n");
+        System.out.println(ipt.length);
+        //System.out.println(numbero.length);
+        System.out.print(ipt);
+        for (int i = 0; i < s.length; i++) {
+            //numbers[i] = Integer.getInteger(s[i]);
+            System.out.println(s[i]);
+        }
+        ConsoleOutputTextArea.append(numbero + ", ");
+        */
     }
 
     //just GUI things
@@ -86,11 +145,13 @@ public class mainApp {
     private void uiHandler(int uiState){
         if(uiState == 0){
             generateNumberButton.setEnabled(true);
+            fromList.setEnabled(true);
             sortTypeComboBox.setEnabled(false);
             startSortButton.setEnabled(false);
             resetButton.setEnabled(false);
         }else if (uiState == 1){
             generateNumberButton.setEnabled(false);
+            fromList.setEnabled(false);
             sortTypeComboBox.setEnabled(true);
             startSortButton.setEnabled(true);
         }else if (uiState == 2){
@@ -117,7 +178,7 @@ public class mainApp {
                     numbers[j + 1] = temp;
                 }
             }
-            ConsoleOutputTextArea.append("Nach Durchlauf " + i + ": ");
+            ConsoleOutputTextArea.append("After run " + i + ": ");
             printToTextArea();
         }
     }
@@ -136,7 +197,7 @@ public class mainApp {
             numbers[pos] = numbers[i];
             numbers[i] = temp;
 
-            ConsoleOutputTextArea.append("Nach Durchlauf " + i +": ");
+            ConsoleOutputTextArea.append("After run " + i +": ");
             printToTextArea();
         }
     }
